2aa40bcd7fcb7feb8abd632a3349b3d8ab37e157
PierreSp <pierre.springer@tum.de>
2017-01-12 13:06:21 +0100

    Made pretty class

--- gaussianpProcess.py
+++ gaussianpProcess.py
@@ -4,87 +4,99 @@ Created on Tue Jan 10 13:34:01 2017
 
 @author: pierre
 """
+
+
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 
-class GaussianProcess:
-    def __init__(self):
-        self.init = 1
+class GaussianProcess():
+    """Gaussian Process simulaton for 2D data.
 
-    def prior_sampling(mini=0, maxi=4, stepsize=0.001, sigma=2.5, l=0.2):
-        dp = np.arange(mini, maxi, stepsize)
-        K = np.arange(float(dp.size * dp.size)).reshape(dp.size, dp.size)
-        for i in range(dp.size):
-            for j in range(i, dp.size):
-                K[i, j] = np.log(sigma**2) + (-1*(dp[i]-dp[j])**2 / (2*l**2))
-                K[j, i] = K[i, j]
-        mean = np.array(np.repeat(0, dp.size))
-        rs = np.random.RandomState(5)
-        x1 = rs.multivariate_normal(mean, np.exp(K), 10).T
-        plt.plot(dp, x1)
+    This class calculates a gaussain process using the exponential kernel
+    with length parameter l and variance parameter sigma. More informations
+    about the background can be found in the github readme file
 
-    def calcK(self, dp1, dp2, sigma=2.5, l=0.2):
-        K = np.arange(float(dp1.size * dp2.size)).reshape(dp1.size, dp2.size)
-        for i in range(dp1.size):
-            for j in range(i, dp2.size):
-                K[i, j] = np.log(sigma**2) + (-1*(dp1[i]-dp2[j])**2 / (2*l**2))
+    Args:
+        X_s (np_array): x-values of points to estimate
+        X (np_array): x-values of training points
+        l (float): length variable in the exp kernel
+        sigma (float): variance in the exp kernel
+
+    """
+    def __init__(self, X_s, X, l=0.6, sigma=1):
+        # Set class variables
+        self.sigma = sigma
+        self.l = l
+        self.X = X
+        self.X_s = X_s
+
+        self.fun = lambda x: x
+        self.__calculated = 0
+
+    def add_noise(self, noise):
+        self.noise_level = noise
+        self.K = self.K + (noise * np.identity(self.X.size))
+
+    def _calc_kernel(self, x1, x2):
+        K = np.zeros([x1.size, x2.size])
+        for i in range(x1.size):
+            for j in range(i, x2.size):
+                K[i, j] = ((self.sigma**2) * np.exp(-1 * (x1[i] - x2[j])**2
+                    / (2 * self.l**2)))
                 K[j, i] = K[i, j]
         return K
 
-    def calcKstar(self, dp1, dp2, sigma=2.5, l=0.2):
-        K = np.arange(float(dp1.size * dp2.size)).reshape(dp1.size, dp2.size)
-        for i in range(dp1.size):
-            for j in range(dp2.size):
-                K[i, j] = np.log(sigma**2) + (-1*(dp1[i]-dp2[j])**2 / (2*l**2))
+    def _calc_kernel_nonquad(self, x1, x2):
+        K = np.zeros([x1.size, x2.size])
+        for i in range(x1.size):
+            for j in range(x2.size):
+                K[i, j] = ((self.sigma**2) * np.exp(-1 * (x1[i] - x2[j])**2
+                    / (2 * self.l**2)))
         return K
 
-    def gauss_fun(self, fun, tpoints=10, mini=0, maxi=4, stepsize=0.008,
-                  sigma=2.5, l=0.2):
-        dpstar = np.arange(mini, maxi, stepsize)
-        dp = np.linspace(mini, maxi, tpoints)
-        ftrain = fun(dp)
-        K = self.calcK(dp, dp)
-        Kstar = self.calcKstar(dp, dpstar)
-        Kstarstar = self.calcK(dpstar, dpstar)
-        mean = (np.transpose(Kstar).dot(
-            np.linalg.inv(K))).dot(ftrain.transpose())
-        cov = Kstarstar - (np.transpose(
-            Kstar).dot(np.linalg.inv(K))).dot(Kstar)
-        rs = np.random.RandomState(5)
-        x1 = rs.multivariate_normal(mean, np.exp(cov), 10).T
-        plt.plot(dp, x1)
+    def calc_samples(self, nsamples=10):
+        if self.__calculated > 0:
+            rs = np.random.RandomState(5)
+            return rs.multivariate_normal(self.mean, self.cov, nsamples).T
+        else:
+            print("No values calculates so far")
+            return None
 
-    def Gaussianpoints(self, X, fun, minval=0, maxval=4, nsample=100,
-                       l=0.6, sigma=1):
+    def cond_gaussian(self, fun):
+        self.fun = fun
+        self.K = self._calc_kernel(self.X, self.X)
+        self.Ks = self._calc_kernel_nonquad(X, self.X_s)
+        self.Kss = self._calc_kernel(self.X_s, self.X_s)
+        self.mean = self.Ks.transpose().dot(np.linalg.inv(self.K)).dot(f)
+        self.cov = self.Kss - (self.Ks.transpose().dot(
+            np.linalg.inv(self.K))).dot(self.Ks)
+        self.__calculated = 1
 
-        f = fun(X)
-        K = np.empty([X.size, X.size])
-        for i in range(X.size):
-            for j in range(X.size):
-                K[i, j] = (sigma**2) * np.exp(-1*(X[i]-X[j])**2 / (2*l**2))
-
-        Xs = np.linspace(minval, maxval, nsample)
-        Kss = np.empty([Xs.size, Xs.size])
-        for i in range(Xs.size):
-            for j in range(Xs.size):
-                Kss[i, j] = (sigma**2) * np.exp(-1*(Xs[i]-Xs[j])**2 / (2*l**2))
-
-        Ks = np.empty([X.size, Xs.size])
-        for i in range(X.size):
-            for j in range(Xs.size):
-                Ks[i, j] = (sigma**2) * np.exp(-1*(X[i]-Xs[j])**2 / (2*l**2))
-
-        mu = Ks.transpose().dot(np.linalg.inv(K)).dot(f)
-        Kov = Kss - Ks.transpose().dot(np.linalg.inv(K)).dot(Ks)
-
-        rs = np.random.RandomState(5)
-        x1 = rs.multivariate_normal(mu, Kov, 20).T
-
-        plt.plot(Xs, x1, X, fun(X), 'ro', Xs, fun(Xs), '--')
+    def plot(self, nsamples):
+        if (self.__calculated == 0):
+            print("No values calculates so far")
+            return None
+        elif (self.__calculated == 1):
+            yplot = self.calc_samples(nsamples)
+            plt.plot(Xs, yplot, X, self.fun(X), 'ro', Xs, self.fun(Xs), '--')
+            return(plt.show())
+        elif (self.__calculated == 2):
+            yplot = self.calc_samples(nsamples)
+            plt.plot(self.X, yplot)
+            return(plt.show())
 
 
-X = np.array([0, 0.25, 0.5, 0.6, 1, 2.1, 2.2, 2.75, 2.8])
-fun = lambda x: np.sin(x)**2
-GP = GaussianProcess()
-GP.Gaussianpoints(X, fun)
+    def prior_sampling(self):
+        self.mean = np.array(np.repeat(0, dp.size))
+        self.cov = self._calc_kernel(self.X, self.X)
+        self.__calculated = 2
+
+
+if __name__ == '__main__':
+    X = np.random.uniform(0, 2, 10)
+    fun = lambda x: np.sin(x)**2
+    Xs = np.linspace(0, 2, 200)
+    GP = GaussianProcess(Xs, X)
+    GP.cond_gaussian(fun)
